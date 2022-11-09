fb22b3a3445a2f54b564abe55bbae7dfd0dd6f08
greengiant83 <greengiant83@hotmail.com>
2022-11-09 11:18:20 -0600

    added player and tablet spawner

---
+++ Runtime/Tablet/TabletSpawner.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabletSpawner : MonoBehaviour
+{
+    public void SpawnTablet()
+    {
+        Debug.Log("Spawning tablet...");
+    }
+}
