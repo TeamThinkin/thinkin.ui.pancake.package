1c23c57947320d6632530cc1187b3ca39a5ca55c
greengiant83 <greengiant83@hotmail.com>
2023-03-20 17:39:40 -0500

    tablet spawner now only spawns a single tablet

--- Runtime/Tablet/TabletSpawner.cs
+++ Runtime/Tablet/TabletSpawner.cs
@@ -7,11 +7,18 @@ public class TabletSpawner : MonoBehaviour
     [SerializeField] private GameObject TabletPrefab;
     [SerializeField] private Transform TabletPose;
 
+    private Tablet tabletInstance;
+
     public void SpawnTablet()
     {
-        var tablet = Instantiate(TabletPrefab);
-        tablet.gameObject.name = "Tablet (" + Time.time.GetHashCode() + ")";
-        tablet.transform.position = TabletPose.position;
-        tablet.transform.rotation = TabletPose.rotation;
+        if (tabletInstance == null)
+        {
+            tabletInstance = Instantiate(TabletPrefab).GetComponent<Tablet>();
+            tabletInstance.gameObject.name = "Tablet (" + Time.time.GetHashCode() + ")";
+        }
+        else tabletInstance.gameObject.SetActive(true);
+
+        tabletInstance.transform.position = TabletPose.position;
+        tabletInstance.transform.rotation = TabletPose.rotation;
     }
 }
